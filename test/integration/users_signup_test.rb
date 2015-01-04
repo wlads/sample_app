4dde49d94d78fc135c5925081b9202f88ec679df
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2015-01-04 20:54:03 -0200

    finish ch 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
@@ -21,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "asdfasdf" }
     end
     assert_template 'users/show'
+    # assert_not flash.now, nil
   end
 end
