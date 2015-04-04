36ed8a1f7b00e93bec785f27f476069658dc44aa
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2015-04-04 10:58:43 -0300

    finish add remember to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
   validates(:name, presence: true)
   validates(:name, length: { maximum: 50 })
   
@@ -17,6 +19,15 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine::cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
 
