24509a326c5a9e85be00ed6f576b375ee90d78e6
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2015-04-05 14:29:08 -0300

    finish ch8

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,15 +14,16 @@ class User < ActiveRecord::Base
   validates(:password, length: { minimum: 6 })
   has_secure_password
 
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine::cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine::cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  def User.new_token
-    SecureRandom.urlsafe_base64
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   def remember
