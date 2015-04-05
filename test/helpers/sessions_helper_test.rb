d7e65e3db5ca90e166fcc5984a1c1b6ece51f0a6
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2015-04-05 02:21:08 -0300

    finish log in/log out

---
+++ test/helpers/sessions_helper_test.rb
@@ -0,0 +1,22 @@
+# 1. Define a user variable using the fixtures.
+# 2. Call the remember method to remember the given user.
+# 3. Verify that current_user is equal to the given user.
+
+require 'test_helper'
+
+class SessionsHelperTest < ActionView::TestCase
+  def setup
+    @user = users(:michael)
+    remember(@user)
+  end
+
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
+  end
+
+  test "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
+  end
+end
