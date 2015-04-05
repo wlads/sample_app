d76b822244fbcfd8f68dfe1308cf3d4994131a73
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2015-04-05 16:38:17 -0300

    finish 9.3

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name: '',
@@ -16,6 +17,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
@@ -24,6 +26,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                     email: email,
                                     password: '',
                                     password_confirmation: '' }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name: name,
+                                    email: email,
+                                    password: 'foobar',
+                                    password_confirmation: 'foobar' }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
